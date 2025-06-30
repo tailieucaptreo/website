b73568411fdac772be7ef6ac018b9a9cf0c03f7a
tailieucaptreo <levantrong011091@gmail.com>
2025-06-30 09:13:38 +0700

    Fix login + connect API + add axios config

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 
-export default axios.create({
-  baseURL: "https://file-upload-backend-u40z.onrender.com/api",
-  withCredentials: true, // nếu backend cần gửi cookie
+const api = axios.create({
+  baseURL: "https://file-upload-backend-u40z.onrender.com/api", // thay bằng URL backend của bạn
 });
+
+export default api;
